231c6e9ec7db5ab63024d25196e1f07759c4c025
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2022-10-19 22:21:17 -0500

    Se agregan configuraciones de la base de datos

--- back/src/database.ts
+++ back/src/database.ts
@@ -1,1 +1,28 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
+import config from "./config";
+
+(async () => {
+
+    user: config.MONGO_USER
+    pass: config.MONGO_PASSWORD 
+
+
+    await mongoose.connect('mongodb://localhost/mern-database');
+    console.log('database is connected') 
+
+})()
+
+
+
+/*import mongoose, { ConnectionOptions } from 'mongoose'
+
+(async () => {
+
+    const mongooseOptions: ConnectionOptions = {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+    }
+    const db = await mongoose.connect('mongodb://localhost/mern-database', mongooseOptions);
+    console.log('database is connected to:', db.connection.name)
+}) () */
+
