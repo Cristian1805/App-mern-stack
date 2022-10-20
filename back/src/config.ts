35f870ce9936d3a7d26bf5358c6c2e3fbb450f4b
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2022-10-19 22:25:37 -0500

    Se añade archivo dotenv para leer las variables de entorno

--- back/src/config.ts
+++ back/src/config.ts
@@ -1,6 +1,16 @@
+import dotenv from 'dotenv'
+dotenv.config();
+
+
+console.log(process.env.Hello)
+
+
 export default {
     MONGO_DATABASE: 'videosdb',
     MONGO_USER: 'admin',
     MONGO_PASSWORD: 'admin',
     MONGO_HOST: 'localhost'
 }
+
+
+//Continuar minuto 36
